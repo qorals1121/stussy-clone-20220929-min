55bd470b4906d9afe38cd20717e4e3e02b033aba
qorals1121 <qorals1121@naver.com>
2022-09-29 12:58:20 +0900

    20220929 수업

---
+++ src/main/java/com/stussy/stussyclone20220929min/StussyClone20220929MinApplication.java
@@ -0,0 +1,13 @@
+package com.stussy.stussyclone20220929min;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StussyClone20220929MinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StussyClone20220929MinApplication.class, args);
+	}
+
+}
